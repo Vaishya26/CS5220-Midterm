d0ddf8e0e5893a1093d277121cedbd52a9c8cfa7
anirudha-bs <bsanirudha.bs@gmail.com>
2024-10-25 12:31:17 -0700

    Fixed bugs and built frontend

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,13 +1,12 @@
 import admin from 'firebase-admin';
-import { createRequire } from 'module'; // Needed for ES module support
+import { createRequire } from 'module'; 
 const require = createRequire(import.meta.url);
 
-const serviceAccount = require('/home/user/CS5220-Midterm/config/serviceAccountKey.json');
+const serviceAccount = require('/home/abs/adv_web/CS5220-Midterm/config/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://cs5220-f93d0-default-rtdb.firebaseio.com",
-  storageBucket: 'cs5220-f93d0.appspot.com',  // Replace with your actual project bucket name
+  storageBucket: 'cs5220-f93d0.appspot.com',
 });
 
 const db = admin.firestore();
